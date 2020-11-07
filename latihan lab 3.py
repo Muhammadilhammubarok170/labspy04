7b3478d20b6ba17418da5c2e057224fb387ae0b4
Muhammadilhammubarok170 <72774571+Muhammadilhammubarok170@users.noreply.github.com>
2020-11-07 21:17:08 +0700

    Add files via upload

---
+++ latihan lab 3.py
@@ -0,0 +1,7 @@
+print('Menampilkan n bilangan acak yang lebih kecil dari 0,5')
+from random import random
+jumlah =int (input("masukan nilai n : "))
+for i in range(5):
+    bil = random()
+    # mencetak bilangan random
+   5 print(bil)
